8b87751004030020765af6799e76ffa9159481d8
Elias <eliascamilo70@outlook.com>
2024-04-19 16:18:29 -0300

    Schema that contains an image property

---
+++ index.js
@@ -0,0 +1,22 @@
+const schemaConfig = {
+  class: 'Meme',
+  vectorizer: 'img2vec-neural',
+  vectorIndexType: 'hnsw',
+  moduleConfig: {
+    'img2vec-neural': {
+      imageFields: ['image'],
+    },
+  },
+  properties: [
+    {
+      name: 'image',
+      dataType: ['blob'],
+    },
+    {
+      name: 'text',
+      dataType: ['string'],
+    },
+  ],
+}
+
+await client.schema.classCreator().withClass(schemaConfig).do()
